546b953b98d24733725bb5498d354ba97a01d777
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2025-02-08 20:54:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const player = {
 function loadSprite(spriteType, count) {
     for (let i = 1; i <= count; i++) {
         const img = new Image();
-        img.src = './img/'+spriteType+'('+i+')'+'.png';
+        img.src = '/img/'+spriteType+'('+i+').png';
         player.sprite[spriteType].push(img);
     }
 }
